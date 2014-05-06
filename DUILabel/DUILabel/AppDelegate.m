67b0cc3607571dd997799c168b04df76e121aaa3
nyan <anjuke@78acc0547dd8.anjuke.net>
2014-05-06 10:48:19 +0800

    first

--- DUILabel/DUILabel/AppDelegate.m
+++ DUILabel/DUILabel/AppDelegate.m
@@ -29,6 +29,7 @@
     [self.window setRootViewController:nav];
     [nav release];
     
+    NSLog(@"");
     
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
